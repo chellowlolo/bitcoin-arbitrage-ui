385feb32e78a618f887065b8508360d164fea5f1
Ryan Casey <ryepdx@gmail.com>
2013-10-31 15:32:28 -0700

    Wired up the routes for the settings, markets, and live feed pages.

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-import json
+import config, markets
 from flask import Flask, flash, redirect, request, render_template
 
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-path_to_config = "../bitcoin-arbitrage/arbitrage/config.json"
 
-@app.route("/")
-def show_config():
+@app.route("/feed", methods=["GET"])
+def show_feed():
+    return render_template("feed.html", port = 8888) 
 
-    with open(path_to_config, "r") as f:
-        config = json.loads(f.read())
+@app.route("/settings", methods=["GET"])
+def show_settings():
+    return render_template("settings.html", config = config.get())
 
-    return render_template("index.html", config = config)
 
-
-@app.route("/update", methods=["POST"])
-def update_config():
-    with open(path_to_config, "r") as f:
-        config = json.loads(f.read())
-
-    with open(path_to_config, "w+") as f:
-        config.update(request.form.to_dict())
-        f.write(json.dumps(config))
-
+@app.route("/settings", methods=["POST"])
+def update_settings():
+    config.update(request.form.to_dict())
     flash("Updated bot settings.")
 
-    return redirect("/")
-    
+    return redirect("/settings")
+
+
+@app.route("/markets", methods=["GET"])
+def show_markets():
+    return render_template("markets.html",
+        markets = config.get().get("markets", {}), all_markets = markets.ALL_MARKETS
+    )
+ 
+@app.route("/markets", methods=["POST"])
+def update_markets():
+    markets = {}
+    for market_currency_pair in request.form.getlist("markets"):
+        market, currency1, currency2 = tuple(market_currency_pair.split("_"))
+        if market not in markets:
+            markets[market] = []
+        markets[market].append((currency1, currency2))
+
+    config.update({"markets": markets})
+    flash("Updated markets being watched.")
+
+    return redirect("/markets")
+
+
 if __name__ == "__main__":
     app.run(debug=True) 
