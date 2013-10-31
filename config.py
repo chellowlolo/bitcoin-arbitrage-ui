cb958b076af2bd17abf11bdc741f40fe18d67ab7
Ryan Casey <ryepdx@gmail.com>
2013-10-31 15:31:10 -0700

    Created a module for updating and getting values from the bitcoin-arbitrage config.json file (which is, for now, assumed to be in a parallel directory.)

---
+++ config.py
@@ -0,0 +1,28 @@
+import json
+
+path_to_config = "../bitcoin-arbitrage/arbitrage/config.json"
+
+def update(new_config):
+    with open(path_to_config, "r") as f:
+        try:
+            config = json.loads(f.read())
+        except ValueError:
+            config = {}
+
+    with open(path_to_config, "w+") as f:
+        for key, value in new_config.items():
+            try:
+                new_config[key] = int(value)
+            except (ValueError, TypeError):
+                try:
+                    new_config[key] = float(value)
+                except (ValueError, TypeError):
+                    new_config[key] = value
+
+        config.update(new_config)
+        f.write(json.dumps(config))
+
+def get():
+    with open(path_to_config, "r") as f:
+        config = json.loads(f.read())
+    return config
